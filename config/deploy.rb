cc987b12073bfaa1bbb8e1103cd2add07063f69a
DESKTOP-Q8C5CS1\ykino <ykinoshi1265@gmail.com>
2018-04-15 15:34:52 +0900

    to_S3_again
    to_S3_again

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,11 +14,10 @@ set :ssh_options, auth_methods: ['publickey'],
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
-
+set :linked_files, %w{ config/secrets.yml }
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
   task :restart do
     invoke 'unicorn:restart'
   end
 end
-set :linked_files, %w{ config/secrets.yml }
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,1 +1,4 @@
 server '54.249.167.42', user: 'ec2-user', roles: %w{app db web}
+set :ssh_options, {
+  keys: %w(/vagrant/.ssh/atwill_new.pem)
+}
