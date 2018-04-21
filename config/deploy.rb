5dc66d21bd4b87577a81bcdae8e453d7aac95f4e
atwill <ykinoshi1265@gmail.com>
2018-04-22 00:33:36 +0900

    for_mac

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/ykino/vagrant/.ssh/atwill_new.pem']
+                  keys: ['/Users/kinoshitayuuta/.ssh/Milk.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.249.167.42', user: 'ec2-user', roles: %w{app db web}
+server '13.115.143.92', user: 'ec2-user', roles: %w{app db web}
 set :ssh_options, {
   keys: %w(/vagrant/.ssh/atwill_new.pem)
 }
